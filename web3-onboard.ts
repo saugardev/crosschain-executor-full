1632876c8e996554492cce8c5f460547530d7135
Saúl García Céspedes <saul@dixwe.com>
2024-06-01 17:18:13 +0200

    chore: configure across compatible chains

--- web3-onboard.ts
+++ web3-onboard.ts
@@ -17,9 +17,6 @@ import tahoWalletModule from '@web3-onboard/taho'
 
 import { init } from '@web3-onboard/react'
 
-// Example key • Replace with your infura key
-const INFURA_KEY = '2996ff3d1a1142689324a8341cb75c68'
-
 const injected = injectedModule({
   custom: [
     // include custom injected wallet modules here
@@ -89,61 +86,62 @@ export default init({
   // An array of Chains that your app supports
   chains: [
     {
-      // hex encoded string, eg '0x1' for Ethereum Mainnet
-      id: '0x1',
-      // string indicating chain namespace. Defaults to 'evm' but will allow other chain namespaces in the future
+      id: 1,
       namespace: 'evm',
-      // the native token symbol, eg ETH, BNB, MATIC
       token: 'ETH',
-      // used for display, eg Ethereum Mainnet
-      label: 'Ethereum Mainnet',
-      // used for network requests
-      rpcUrl: `https://mainnet.infura.io/v3/${INFURA_KEY}`
+      label: 'Ethereum',
+      rpcUrl: `https://eth.llamarpc.com`
+    },
+    {
+      id: 10,
+      token: 'ETH',
+      label: 'Optimism',
+      rpcUrl: `https://optimism.llamarpc.com`
+    },
+    {
+      id: 137,
+      token: 'MATIC',
+      label: 'Polygon',
+      rpcUrl: 'https://matic-mainnet.chainstacklabs.com'
+    },
+    {
+      id: 324,
+      token: 'ETH',
+      label: 'zkSync',
+      rpcUrl: 'https://zksync-era.blockpi.network/v1/rpc/public'
+    },
+    {
+      id: 8453,
+      token: 'ETH',
+      label: 'Base',
+      rpcUrl: 'https://base.llamarpc.com'
     },
     {
       id: 42161,
-      token: 'ARB-ETH',
+      token: 'ETH',
       label: 'Arbitrum One',
       rpcUrl: 'https://rpc.ankr.com/arbitrum'
     },
     {
-      id: '0xa4ba',
-      token: 'ARB',
-      label: 'Arbitrum Nova',
-      rpcUrl: 'https://nova.arbitrum.io/rpc'
-    },
-    {
-      id: '0x89',
-      token: 'MATIC',
-      label: 'Matic Mainnet',
-      rpcUrl: 'https://matic-mainnet.chainstacklabs.com'
-    },
-    {
-      id: '0xa4ec',
+      id: 59144,
       token: 'ETH',
-      label: 'Celo',
-      rpcUrl: 'https://1rpc.io/celo'
+      label: 'Linea',
+      rpcUrl: 'https://linea.blockpi.network/v1/rpc/public'
     },
-    {
-      id: 666666666,
-      token: 'DEGEN',
-      label: 'Degen',
-      rpcUrl: 'https://rpc.degen.tips'
-    }
   ],
   appMetadata: {
     // The name of your dApp
-    name: 'Blocknative',
+    name: 'Crosschain Executor',
     // SVG icon string, with height or width (whichever is larger) set to 100% or a valid image URL
     icon: '<svg></svg>',
     // Optional wide format logo (ie icon and text) to be displayed in the sidebar of connect modal. Defaults to icon if not provided
     logo: '<svg></svg>',
     // The description of your app
-    description: 'Demo app for Onboard V2',
+    description: 'Demo app for Across+',
     // The url to a getting started guide for app
-    gettingStartedGuide: 'http://mydapp.io/getting-started',
+    gettingStartedGuide: 'https://github.com/saugardev/crosschain-executor/blob/main/README.md',
     // url that points to more information about app
-    explore: 'http://mydapp.io/about',
+    explore: 'https://github.com/saugardev/crosschain-executor',
     // if your app only supports injected wallets and when no injected wallets detected, recommend the user to install some
     recommendedInjectedWallets: [
       {
@@ -152,7 +150,6 @@ export default init({
         // link to download wallet
         url: 'https://metamask.io'
       },
-      { name: 'Coinbase', url: 'https://wallet.coinbase.com/' }
     ],
     // Optional - but allows for dapps to require users to agree to TOS and privacy policy before connecting a wallet
     agreement: {
